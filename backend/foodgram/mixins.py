e5bae5542477d402912187f340f9ee035cae708b
Savelev Kalin <kalin.savelev07@gmail.com>
2022-01-23 12:52:31 +0800

    Delete backend directory

--- backend/foodgram/mixins.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import mixins, viewsets
-
-
-class MixinsSet(mixins.ListModelMixin,
-                mixins.RetrieveModelMixin,
-                viewsets.GenericViewSet):
-    pass
